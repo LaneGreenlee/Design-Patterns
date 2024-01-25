21cce183cec63343695fdb57e4c2fbb412a6b801
LaneGreenlee <lanegreenlee9@gmail.com>
2024-01-25 15:49:14 -0500

    Made changes to make base warrior print.

--- decorator/Player.java
+++ decorator/Player.java
@@ -5,16 +5,20 @@ public class Player {
     protected String name;
     protected ArrayList<String> lines;
 
-
     public Player(ArrayList<String> lines, String name) {
-
+        this.name = name;
+        this.lines = lines;
     }
 
     public String getName() {
-        
+        return name;
     }
 
     public String toString() {
-
+        String visual = "##### "+getName()+" #####\n";
+        for (String line : lines) {
+            visual+=line+"\n";
+        }
+        return visual;
     }
 }
--- decorator/Warrior.java
+++ decorator/Warrior.java
@@ -1,5 +1,8 @@
 package decorator;
 
-public class Warrior {
+public class Warrior extends Player {
     
+    public Warrior(String name) {
+        super(FileReader.getLines("decorator/txt/warrior.txt"),name);
+    }
 }
