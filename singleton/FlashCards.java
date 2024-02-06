60c4f5bbe383f679852eab66317f40045bba68fa
LaneGreenlee <lanegreenlee9@gmail.com>
2024-02-06 18:37:16 -0500

    added javadoc

--- singleton/FlashCards.java
+++ singleton/FlashCards.java
@@ -1,20 +1,41 @@
 import java.util.Random;
 import java.util.ArrayList;
+/**
+ * Class used when a flashcard needs to be made
+ * @auth Laneag
+ */
 public class FlashCards {
     private Random rand;
-    private FlashCards flashCards;
+    private static FlashCards flashCards;
     private ArrayList<Word> words;
     private Word currentWord;
 
-    private FlashCards(){}
+    /**
+     * Constructs a flashcard and creates random object
+     * and populates words ArrayList
+     */
+    private FlashCards(){
+        words = FileReader.getWords();
+        rand = new Random();
+    }
 
-    public FlashCards getInstance() {
+    /**
+     * Static method of getintance, only makes
+     * one insatance of flashcards
+     * @return FlashCards
+     */
+    public static FlashCards getInstance() {
         if( flashCards == null) 
             flashCards = new FlashCards();
         
         return flashCards;
     }
+    /**
+     * gets random word from arraylist using rand
+     * @return the current word
+     */
     public Word getWord() {
+        currentWord = words.get(rand.nextInt(words.size()));
         return currentWord;
     }
     
--- singleton/Word.java
+++ singleton/Word.java
@@ -4,16 +4,34 @@ public class Word {
     private String definition;
     private String sentence;
 
-
+    /**
+     * Sets the word attributes to ones passed into constructor
+     * @param word The word you'd like to define.
+     * @param type The type of the word noun, adj, etc.
+     * @param definition definition of the word.
+     * @param sentence example of a sentence using the word.
+     */
     public Word(String word, String type, String definition, String sentence) {
-
+        this.word = word;
+        this.type = type;
+        this.definition = definition;
+        this.sentence = sentence;
     }
 
+    /**
+     * getter method for word
+     * @return A string representation of the word.
+     */
     public String getWord() {
-        return "";
+        return word;
     }
 
+        /**
+         * builds word string that displays to UI
+         * contains all word attribues
+         * @return word attribute representation for UI.
+         */
     public String getDescription() {
-        return "";
+        return "Part of Speech: "+type+"\nDefintion: "+definition+"\n"+sentence;
     }
 }
